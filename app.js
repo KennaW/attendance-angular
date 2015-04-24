6f8369c72f56149e17b7d0006437f6ff24a0b395
KennaW <KennaWarsinske@gmail.com>
2015-04-24 10:09:00 -0700

    add home.html

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 var attendanceSheet = angular.module('attendanceSheet', [ui.router]);
+
+attendanceSheet.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "partials/home.html"
+  });
+
+  //add states
+
+});
