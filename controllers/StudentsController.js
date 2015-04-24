7999db6958f391819c370eacd6c848de6435578c
KennaW <KennaWarsinske@gmail.com>
2015-04-24 15:54:51 -0700

    add factory

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,9 +1,14 @@
-attendanceSheet.controller('attendanceSheet', function StudentsCtrl($scope) {
-  $scope.students = [
-    { name: "Sam Schmidt", here: false },
-    { name: "Jessica Martin", here: false },
-    { name: "Sandy Smith", here: false },
-    { name: "Ryan Samuels", here: false },
-    { name: "Brentwood Davis", here: false }
-  ]
+// attendanceSheet.controller('attendanceSheet', function StudentsCtrl($scope) {
+//   $scope.students = [
+//     { name: "Sam Schmidt", here: false },
+//     { name: "Jessica Martin", here: false },
+//     { name: "Sandy Smith", here: false },
+//     { name: "Ryan Samuels", here: false },
+//     { name: "Brentwood Davis", here: false }
+//   ]
+// });
+
+attendanceSheet.controller('StudentsCtrl', function StudentsCtrl($scope, attendanceSheet) {
+  $scope.students = attendanceFactory.students;
+  $scope.attendanceFactory = attendanceFactory;
 });
---
+++ services/studentsFactory.js
@@ -0,0 +1,9 @@
+attendanceSheet.factory('attendanceFactory', function attendanceFactory($scope) {
+  $scope.students = [
+    { name: "Sam Schmidt", here: false },
+    { name: "Jessica Martin", here: false },
+    { name: "Sandy Smith", here: false },
+    { name: "Ryan Samuels", here: false },
+    { name: "Brentwood Davis", here: false }
+  ]
+});
