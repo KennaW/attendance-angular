0bc5e35ee2a728750383624460c8df3e1afa45fa
KennaW <KennaWarsinske@gmail.com>
2015-04-24 12:04:18 -0700

    fix display error, add signin.html

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-var attendanceSheet = angular.module('attendanceSheet', [ui.router]);
+var attendanceSheet = angular.module('attendanceSheet', ['ui.router']);
 
 attendanceSheet.config(function($stateProvider) {
-  $stateProvider.state('home', {
-    url:"",
+
+  $stateProvider
+
+  .state('home', {
+    url: "",
     templateUrl: "partials/home.html"
+  })
+
+//nested states from home
+  .state('home.signin', {
+    url:"/signin",
+    templateUrl: 'partials/signin.html'
+  })
+
+  .state('home.presentAbsent', {
+    url:"/present",
+    templateUrl: 'partials/presentAbsent.html'
   });
 
   //add states
